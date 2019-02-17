0f91d0177e4c46010b5e17a7b7848886c918dced
Andrés Londoño <andreslon@outlook.com>
2019-02-17 17:08:51 -0500

    Created example project using hateoas and swagger

---
+++ NetCore.Hateoas.Api/Controllers/PeopleController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCore.Hateoas.Api.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NetCore.Hateoas.Api.Controllers
+{
+    [Produces("application/json+hateoas")]
+    [Route("api/[controller]")]
+    public class PeopleController : Controller
+    {
+        private readonly IEnumerable<PersonDto> people;
+        public PeopleController()
+        {
+            people = new List<PersonDto>() {
+                new PersonDto { Id = 1, Name = "Fanie", Email = "fanie@reynders.co" },
+                new PersonDto { Id = 2, Name = "Maarten", Email = "maarten@example.com" },
+                new PersonDto { Id = 3, Name = "Marcel", Email = "marcel@example.com" }
+            };
+        }
+
+        private PersonDto GetPerson(int id)
+        {
+            return people.Single(p => p.Id == id);
+        }
+        [HttpGet(Name = "get-people")]
+        public IActionResult Get()
+        {
+            return Ok(people);
+        }
+
+        [HttpGet("{id}", Name = "get-person")]
+        public IActionResult Get(int id)
+        {
+            var person = GetPerson(id);
+            return Ok(person);
+        }
+        /// <summary>
+        /// Creates a person.
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /People
+        ///     {
+        ///        "id": 1,
+        ///        "name": "andres",
+        ///        "email": "andres@gmail.com"
+        ///     }
+        ///
+        /// </remarks>
+        /// <param name="person"></param>
+        /// <returns>A newly created person</returns>
+        /// <response code="200">Returns the newly created person</response>
+        /// <response code="400">If the item is null</response>
+        [ProducesResponseType(typeof(PersonDto), 200)]
+        [ProducesResponseType(400)]
+        [HttpPost(Name = "create-person")]
+        public IActionResult Post([FromBody]PersonDto person)
+        {
+            person.Id = people.Count() + 1;
+            ((List<PersonDto>)people).Add(person);
+            return Ok(person);
+        }
+
+        [HttpPut("{id}", Name = "update-person")]
+        public IActionResult Put(int id, [FromBody]PersonDto person)
+        {
+            var oldPerson = GetPerson(id);
+
+            oldPerson.Name = person.Name;
+            oldPerson.Email = person.Email;
+            return NoContent();
+        }
+        /// <summary>
+        /// Deletes a specific person.
+        /// </summary>
+        /// <param name="id"></param> 
+        [HttpDelete("{id}", Name = "delete-person")]
+        public IActionResult Delete(int id)
+        {
+            var person = GetPerson(id);
+
+            ((List<PersonDto>)people).Remove(person);
+            return Ok();
+        }
+    }
+}
---
+++ NetCore.Hateoas.Api/Startup.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NetCore.Hateoas.Api.Models;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace NetCore.Hateoas.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "ToDo API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "Shayne Boyer",
+                        Email = string.Empty,
+                        Url = "https://twitter.com/spboyer"
+                    },
+                    License = new License
+                    {
+                        Name = "Use under LICX",
+                        Url = "https://example.com/license"
+                    }
+                });
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+            services
+                .AddMvc()
+                .AddHateoas(options =>
+                {
+                    options
+                       .AddLink<PersonDto>("get-person", p => new { id = p.Id })
+                       .AddLink<List<PersonDto>>("create-person")
+                       .AddLink<PersonDto>("update-person", p => new { id = p.Id })
+                       .AddLink<PersonDto>("delete-person", p => new { id = p.Id });
+                })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
